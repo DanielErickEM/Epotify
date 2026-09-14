6809d8617c40fd119fef77b273df7567bd4cd4f2
DanielErickEM <7xdaniel1234@gmail.com>
2026-09-13 19:45:39 -0500

    Personalizar base de Epotify

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.gokadzev.musify"
+    namespace = "com.danielerickem.epotify"
     compileSdk = 36
     ndkVersion = "28.2.13676358"
 
@@ -48,7 +48,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.gokadzev.musify"
+        applicationId = "com.danielerickem.epotify"
         minSdk = 24
         targetSdk = 36
         versionCode = flutter.versionCode
